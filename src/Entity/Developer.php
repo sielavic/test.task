de594dd2cdbd78aea1771ac12d2995a1e71e2e52
sielavic <63396872+sielavic@users.noreply.github.com>
2024-08-25 16:13:43 +0300

    пункт № 1 выполнен

---
+++ src/Entity/Developer.php
@@ -0,0 +1,75 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="developers")
+ */
+class Developer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fullName;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $position; // возможные значения: программист, администратор, devops, дизайнер
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    private $contactPhone;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Project", mappedBy="developers")
+     */
+    private $projects;
+
+    public function __construct()
+    {
+        $this->projects = new ArrayCollection();
+    }
+
+
+    public function getProjects(): Collection
+    {
+        return $this->projects;
+    }
+
+    public function addProject(Project $project): self
+    {
+        if (!$this->projects->contains($project)) {
+            $this->projects[] = $project;
+            $project->addDeveloper($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProject(Project $project): self
+    {
+        if ($this->projects->removeElement($project)) {
+            $project->removeDeveloper($this);
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Project.php
@@ -0,0 +1,67 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="projects")
+ */
+class Project
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Developer", inversedBy="projects")
+     * @ORM\JoinTable(name="developers_projects")
+     */
+    private $developers;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $customer;
+
+    public function __construct()
+    {
+        $this->developers = new ArrayCollection();
+    }
+
+
+
+    public function getDevelopers(): Collection
+    {
+        return $this->developers;
+    }
+
+    public function addDeveloper(Developer $developer): self
+    {
+        if (!$this->developers->contains($developer)) {
+            $this->developers[] = $developer;
+            $developer->addProject($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDeveloper(Developer $developer): self
+    {
+        if ($this->developers->removeElement($developer)) {
+            $developer->removeProject($this);
+        }
+
+        return $this;
+    }
+}
